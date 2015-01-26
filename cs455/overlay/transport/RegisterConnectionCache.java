f5f14626f4b58e39c1af693a652e7d1e98965d58
wHome <wbarras@rams.colostate.edu>
2015-01-26 08:02:36 -0700

    Began writing RegisterConnectionClass. It's time for class, so I'm out

---
+++ cs455/overlay/transport/RegisterConnectionCache.java
@@ -0,0 +1,12 @@
+package cs455.overlay.transport;
+/*
+ *Author: Tiger Barras
+ *RegisterConnectionCache.java
+ *Wrapper for a socket array that holds all the nodes that talk to the register
+ */
+
+public class RegisterConnectionCache implements ConnectionCache{
+
+
+
+}//End class
