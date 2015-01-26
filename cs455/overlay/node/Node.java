7f042e7f242e392e1709a95ccfa1d3081c605cb2
wHome <wbarras@rams.colostate.edu>
2015-01-25 22:30:40 -0700

    Started to get a shape for Node.java Wrote a method that listens at a port and returns a socket This assignment is way bigger than I thought...

--- cs455/overlay/node/Node.java
+++ cs455/overlay/node/Node.java
@@ -4,11 +4,20 @@ package cs455.overlay.node;
 //Node class. Will be inherited by Registry and MessagingNode classes
 
 import java.net.ServerSocket;
+import java.io.*;
+import java.util.arrayList;
 
 public class Node{
+
+  //Listens at a specific port, and then passes out a Socket
+  public Socket listen(int portNum) throws IOException{
+    serverSocket = new ServerSocket(portNum);
+    System.out.println("Waiting for connection on port " + portnum);
+    Socket socket = serverSocket.accept();
+    return socket;
+  }//end listen
 
   public static void main(String args[]){
 
   }
-
 }
