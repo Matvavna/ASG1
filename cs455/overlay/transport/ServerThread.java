e8fcb622eb6278b88dd5960c03d9ecf79e02e75b
wHome <wbarras@rams.colostate.edu>
2015-01-27 21:23:11 -0700

    Made it so ServerThread starts a new recieverThread for each new socket instead of adding them to a cache

--- cs455/overlay/transport/ServerThread.java
+++ cs455/overlay/transport/ServerThread.java
@@ -6,19 +6,22 @@ package cs455.overlay.transport;
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.io.IOException;
+import cs455.overlay.transport.RecieverThread;
 
 //I think this needs to run on port 0, which will make it find an acceptable port
 
 public class ServerThread extends Thread{
 
 	int portNum; //Port that the ServerThread object will listen on
-	ConnectionCache cache;
+	//ConnectionCache cache;
 	long id;
 	String name;
 
-	public ServerThread(int pn, ConnectionCache c){ //Port number to listen to, cache to add sockets to
+	//I don't think this needs to have a ConnectionCache associated with it
+	//All those links need is a new RecieverThread
+	public ServerThread(int pn){ //Port number to listen to, cache to add sockets to
 		portNum = pn;
-		cache = c;
+		//cache = c;
 		id = this.getId();
 		name = this.getName();
 	}//End constructor
@@ -27,13 +30,16 @@ public class ServerThread extends Thread{
 	public void run(){
 		Socket socket;
 		try{ //Listen at port portNum, and open socket to an incoming connection
-			ServerSocket serverSocket = new ServerSocket(portNum);
-			messageWithId("Ready to connect. . .");
-			socket = serverSocket.accept();
-			messageWithId("Socket Generated");
-			cache.add(1,socket);
-			messageWithId("Socket added to cache");
-			//Open up new Connection
+			while(true){
+				ServerSocket serverSocket = new ServerSocket(portNum);
+				messageWithId("Ready to connect. . .");
+				socket = serverSocket.accept();
+				messageWithId("Socket Generated");
+				RecieverThread rt = new RecieverThread(socket);
+				rt.start();
+				messageWithId("RecieverThread started to handle new link");
+				//Open up new Connection
+			}
 		}catch(IOException e){
 			messageWithId("Error opening Server Socket");
 			messageWithId("Error: " + e);
