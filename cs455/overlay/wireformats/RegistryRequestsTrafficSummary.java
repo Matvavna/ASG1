a98f29373bc4c28df638957d76d6c41bd8f7bfd6
wHome <wbarras@rams.colostate.edu>
2015-02-17 11:20:55 -0700

    Added RRTS constructor

--- cs455/overlay/wireformats/RegistryRequestsTrafficSummary.java
+++ cs455/overlay/wireformats/RegistryRequestsTrafficSummary.java
@@ -20,10 +20,26 @@ import java.io.IOException;
 
 public class RegistryRequestsTrafficSummary implements Event{
 
-	private byte[] message = {11};
+	private byte[] message;
 	private int messageType = 11;
 
+	public RegistryRequestsTrafficSummary(){
+		try{
+			message = null;
+			ByteArrayOutputStream baOutputStream = new ByteArrayOutputStream();
+			DataOutputStream dout = new DataOutputStream(new BufferedOutputStream(baOutputStream));
 
+			dout.writeInt(messageType);
+
+			dout.flush();
+			message = baOutputStream.toByteArray();
+			baOutputStream.close();
+			dout.close();
+		}catch(IOException e){
+			System.out.println("RRTS: Error Marshalling");
+			System.out.println(e);
+		}
+	}
 
 	public byte[] getBytes(){
 		return message;
